c699f41682571ffbac3e53bbecffaaffcef84751
Gianluca Baldin <gianlucabaldin86@gmail.com>
2020-02-14 00:16:22 +0100

    Inserted attempt structure in Board (WIP)

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table, Row, Col, Button } from "react-bootstrap";
+import { Table, Row, Col, Button, Form } from "react-bootstrap";
 import { connect } from "react-redux";
 import { start, abort } from "../store/actions/mastermindActions";
 import PropTypes from "prop-types";
@@ -14,7 +14,7 @@ const Board = ({ playing, start, abort }) => {
           </Button>
         </Col>
       </Row>
-    )
+    );
   } else {
     return (
       <>
@@ -23,20 +23,36 @@ const Board = ({ playing, start, abort }) => {
             <Table>Table (TODO)</Table>
           </Col>
         </Row>
-        <Row>
-        <Col>
+
+        <Form>
+          <Form.Row>
+            <Form.Group as={Col} xs={3} controlId="formGridEmail">
+              <Form.Label >Insert your attempt</Form.Label>
+            </Form.Group>
+            <Form.Group as={Col} xs={2} controlId="formGridEmail">
+              {/* <Form.Label>Email</Form.Label> */}
+              <Form.Control type="text" placeholder="number 1" />
+            </Form.Group>
+            <Form.Group as={Col} xs={2} controlId="formGridEmail">
+              {/* <Form.Label>Email</Form.Label> */}
+              <Form.Control type="text" placeholder="number 2" />
+            </Form.Group>
+            <Form.Group as={Col} xs={2} controlId="formGridEmail">
+              {/* <Form.Label>Email</Form.Label> */}
+              <Form.Control type="text" placeholder="number 3" />
+            </Form.Group>
+          </Form.Row>
           <Button variant="primary" onClick={() => abort()}>
             Abort
           </Button>
-        </Col>
-      </Row>
+        </Form>
       </>
     );
   }
 };
 
 const mapStateToProps = ({ reducer }) => ({
-  playing: reducer.playing,
+  playing: reducer.playing
 });
 
 const mapDispatchToProps = {
