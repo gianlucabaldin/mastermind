22b5e3bfd03f48e1b2abe81f38a4a29d76856d66
Gianluca Baldin <gianlucabaldin86@gmail.com>
2020-02-18 22:58:40 +0100

    BS styles + fix submit method

--- src/components/Attempt.jsx
+++ src/components/Attempt.jsx
@@ -3,6 +3,7 @@ import { Col, Button, Form } from "react-bootstrap";
 import { connect } from "react-redux";
 import { abort, insertAttempt } from "../store/actions/mastermindActions";
 import PropTypes from "prop-types";
+import "../style/attempt.css";
 
 const Attempt = ({ abort, insertAttempt }) => {
   const [validated, setValidated] = useState(false);
@@ -10,17 +11,21 @@ const Attempt = ({ abort, insertAttempt }) => {
   const handleSubmit = event => {
     const form = event.currentTarget;
     event.preventDefault();
+
     if (form.checkValidity() === false) {
       event.stopPropagation();
-      setValidated(false);
+      setValidated(true);
+      return;
     } else {
       insertAttempt([
         Number(form.elements.n1.value),
         Number(form.elements.n2.value),
         Number(form.elements.n3.value)
       ]);
+      setValidated(false);
+      form.reset(); // blank the values
+      return;
     }
-    setValidated(true);
   };
 
   return (
