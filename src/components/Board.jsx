0a136a261cef5a132a3cea3c899bdae2a5a84b2e
Gianluca Baldin <gbaldin@intesys.it>
2020-02-18 12:35:20 +0100

    renamed Cols / Table dynamic visibility

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Table, Row, Col, Button } from "react-bootstrap";
 import Attempt from "./Attempt";
 import { connect } from "react-redux";
-import { start, abort } from "../store/actions/mastermindActions";
+import { start, abort, attempt } from "../store/actions/mastermindActions";
 import PropTypes from "prop-types";
-import {checkRightNumber, checkRightPosition} from '../shared/attempts'
+import { checkRightNumber, checkRightPosition } from "../shared/attempts";
 
 const Board = ({ playing, start, abort, attempts, secretTriad }) => {
   if (!playing) {
@@ -20,34 +20,37 @@ const Board = ({ playing, start, abort, attempts, secretTriad }) => {
   } else {
     return (
       <>
-        <Row>
-          <Col>
-            <Table>
-              <thead>
-                <tr>
-                  <td>Attempt</td>
-                  <td>Number 1</td>
-                  <td>Number 2</td>
-                  <td>Number 3</td>
-                  <td>Right number</td>
-                  <td>Right position</td>
-                </tr>
-              </thead>
-              <tbody>
-                {attempts.map((attempt, index) => (
+        <Row>secretTriad = {secretTriad}--> REMOVE ME</Row>
+        {attempts && attempts.length > 0 && (
+          <Row>
+            <Col>
+              <Table>
+                <thead>
                   <tr>
-                    <td>{index + 1}</td>
-                    <td>{attempt[0]}</td>
-                    <td>{attempt[1]}</td>
-                    <td>{attempt[2]}</td>
-                    <td>{checkRightNumber(secretTriad, attempt)}</td>
-                    <td>{checkRightPosition(secretTriad, attempt)}</td>
+                    <td>Attempt</td>
+                    <td>N.1</td>
+                    <td>N.2</td>
+                    <td>N.3</td>
+                    <td>Right number</td>
+                    <td>Right position</td>
                   </tr>
-                ))}
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
+                </thead>
+                <tbody>
+                  {attempts.map((attempt, index) => (
+                    <tr>
+                      <td># {index + 1}</td>
+                      <td>{attempt[0]}</td>
+                      <td>{attempt[1]}</td>
+                      <td>{attempt[2]}</td>
+                      <td>{checkRightNumber(secretTriad, attempt)}</td>
+                      <td>{checkRightPosition(secretTriad, attempt)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        )}
         <Row>
           <Col>
             <Attempt />
