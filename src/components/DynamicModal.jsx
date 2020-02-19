06f120d59088a06eb75af0a8c75178ab4df0fcd3
Gianluca Baldin <gbaldin@intesys.it>
2020-02-19 11:18:16 +0100

    removed unused var

--- src/components/DynamicModal.jsx
+++ src/components/DynamicModal.jsx
@@ -1,56 +1,46 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { toggleModal, abort } from '../store/actions/mastermindActions'
-import { Modal, Button } from 'react-bootstrap';
-import '../style/modal.css';
-import PropTypes from 'prop-types';
+import React from "react";
+import { connect } from "react-redux";
+import { toggleModal, abort } from "../store/actions/mastermindActions";
+import { Modal, Button } from "react-bootstrap";
+import "../style/modal.css";
+import PropTypes from "prop-types";
 
-const DynamicModal = ({ visible, title, content, confirmButton, toggleModal, abort }) => (
+const DynamicModal = ({ visible, title, content, toggleModal, abort }) => (
   <Modal show={visible} onHide={toggleModal}>
     <Modal.Header closeButton>
       <Modal.Title>{title}</Modal.Title>
     </Modal.Header>
     <Modal.Body>
-      <p>
-        Content: {content}
-      </p>
+      <p>{content}</p>
     </Modal.Body>
     <Modal.Footer>
-      <Button variant="secondary" onClick={confirmButton} className="mr-2" >
-        {confirmButton}
-      </Button>
-      <Button variant="primary" onClick={abort} >
+      <Button variant="primary" onClick={abort}>
         Close
       </Button>
     </Modal.Footer>
-  </Modal >
-)
+  </Modal>
+);
 
 // check types
 Modal.propTypes = {
   // variables
   visible: PropTypes.bool,
   title: PropTypes.string,
-  content: PropTypes.string,
-  confirmButton: PropTypes.string,
+  content: PropTypes.string
   // actions
   toggleModal: PropTypes.func,
   abort: PropTypes.func
-}
+};
 
 const mapStateToProps = ({ reducer }) => ({
   visible: reducer.modal.visible,
   title: reducer.modal.title,
-  content: reducer.modal.content,
-  confirmButton: reducer.modal.confirmButton
-})
+  content: reducer.modal.content
+});
 
 const mapDispatchToProps = {
   toggleModal,
   abort
-}
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(DynamicModal)
+export default connect(mapStateToProps, mapDispatchToProps)(DynamicModal);
