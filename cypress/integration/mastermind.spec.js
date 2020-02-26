9e2e22cd38b0b0f6c2324391de59d433a5d7e9e0
Gianluca Baldin <gbaldin@intesys.it>
2020-02-26 02:06:37 +0100

    added Cypress tests

--- cypress/integration/mastermind.spec.js
+++ cypress/integration/mastermind.spec.js
@@ -1,11 +1,21 @@
 /* eslint-disable no-undef */
 /// <reference types="Cypress" />
 
+/**
+ * Enters the home page and checks home texts
+ */
 const enter = () => {
   cy.server();
   cy.visit("/");
   cy.findByText("Mastermind game");
   cy.get('[data-test="btn-start"]').should("exist");
+};
+
+/**
+ * Clicks on "Start" button
+ */
+const startNewGame = () => {
+  cy.get('[data-test="btn-start"]').click();
 };
 
 const insertAttempt = (n1, n2, n3) => {
@@ -15,6 +25,12 @@ const insertAttempt = (n1, n2, n3) => {
   cy.get('[data-test="insert"]').click();
 };
 
+/**
+ * Checks popup title and content
+ * @param {string} headerText the title text to check
+ * @param {string} firstLineBodyText  the body's first line to check
+ * @param {string} secondLineBodyText the body's second line to check
+ */
 const checkPopup = (headerText, firstLineBodyText, secondLineBodyText) => {
   cy.get(".modal-dialog").should("be.visible");
   if (headerText) {
@@ -28,11 +44,23 @@ const checkPopup = (headerText, firstLineBodyText, secondLineBodyText) => {
   }
 };
 
-const startNewGame = () => {
-  cy.get('[data-test="btn-start"]').click();
+/**
+ * Read from Redux store the real generated secret combination
+ */
+const getSecretValue = () => {
+    cy
+      .window()
+      .its("store")
+      .invoke("getState")
+      .its("reducer")
+      .as("store")
 };
 
+
+/******** TEST SUITE START **********/
+
 context("Mastermind game tests", () => {
+    
   it("Should enter the app", () => {
     enter();
   });
@@ -111,6 +139,30 @@ context("Mastermind game tests", () => {
     startNewGame();
     insertAttempt("1", "1", "3");
     cy.get('[data-test="abort"]').click();
-    checkPopup("Game aborted :(", "You chose to abort the session game", "You secret combination was");
+    checkPopup(
+      "Game aborted :(",
+      "You chose to abort the session game",
+      "You secret combination was"
+    );
+  });
+
+  it("Should guess the combination at first attempt", () => {
+    enter();
+    startNewGame();
+    getSecretValue();
+    cy.get("@store")
+      .its("secretCombination")
+      .then(reduxCombination => {
+        insertAttempt(
+          reduxCombination[0],
+          reduxCombination[1],
+          reduxCombination[2]
+        );
+        checkPopup(
+          "Congratulations :)",
+          "You guessed the number in 1 attempts!",
+          "You took 0 hours, 0 minutes and 0 seconds."
+        );
+      });
   });
 });
